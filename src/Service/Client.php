0635ef0baed45e86a43057f6e3019a9b633e1a0e
Danylo Kabanov <draobrehtom@gmail.com>
2018-09-02 20:09:12 +0200

    Add a warining about creds

--- src/Service/Client.php
+++ src/Service/Client.php
@@ -6,6 +6,11 @@ class Client {
 
     public function __construct(string $url, string $login, string $password)
     {
+        if (empty($login) or empty($password)) {
+            print_r('Please, enter login and password in app.php');
+            exit();
+        }
+
         $this->request = new Request($url, $login, $password);
     }
 
